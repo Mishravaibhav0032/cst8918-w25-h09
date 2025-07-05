9dee34e9ddb4bbd454447bcf999e5ef9987df670
Vaibhav <mish0032@algonquinlive.com>
2025-07-05 00:24:18 -0400

    test hook

---
+++ infrastructure/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-aks-demo"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "aks_cluster_name" {
+  default = "aks-demo-cluster"
+}
